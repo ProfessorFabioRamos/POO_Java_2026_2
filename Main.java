d3491f8c4dd062e9228c3e07effa85c165efc1ef
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-07-27 10:49:56 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args){
+        //System.out.print("Hello World");
+        int idade = 27;
+        float altura = 1.78f;
+        double peso = 87;
+        char categoria = 'B';
+        String nome = "Carlos";
+        boolean cadastroAtivo = true;
+
+        //OPERADORES ARITMETICOS
+        int a = 3;
+        int b = 2;
+        int c;
+
+        c = a + b; // ADIÇÃO
+        c = a - b; // SUBTRAÇÃO
+        c = a * b; // MULTIPLICAÇÃO
+        c = a / b; // DIVISÃO
+        c = a % b; // DIVISÃO RETORNANDO O RESTO
+
+        System.out.print(c);
+    }
+}
