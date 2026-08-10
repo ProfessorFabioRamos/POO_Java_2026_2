5675aafbf7a29c840fc00d4a245a46e105cf9bd0
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-08-10 09:34:55 -0300

    Create Switch_Case.java

---
+++ Revisão/Switch_Case.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        String fruta = "";
+        
+        Scanner sc = new Scanner(System.in); // Declaração do Scanner com UTF-8
+        System.out.print("Digite o nome de uma fruta: ");
+        fruta = sc.nextLine();
+        fruta = fruta.toLowerCase();
+        sc.close();
+
+        switch (fruta) {
+            case "banana":
+                System.out.println("Amarelo");
+                break;
+            case "maca":
+                System.out.println("Vermelho");
+                break;
+            case "abacate":
+                System.out.println("Verde");
+                break;
+            case "acai":
+                System.out.println("Roxo");
+                break;
+            default:
+                System.out.println("Fruta Indisponível");
+                break;
+        }
+    }
+}
