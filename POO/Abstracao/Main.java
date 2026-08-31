1f811edf8813ec2bc738e3ffb21b37da2cdfa2a1
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-08-31 10:46:00 -0300

    Update Main.java

--- POO/Abstracao/Main.java
+++ POO/Abstracao/Main.java
@@ -2,5 +2,14 @@ public class Main{
     public static void main(String[] args){
         Carro carro_1 = new Carro("Honda", "Civic",
          5, "JGF65F90");
+        //System.out.println(carro_1.marca);
+        //carro_1.placa = "JOE65F90";
+        //System.out.println(carro_1.placa);
+
+        System.out.println(carro_1.getMarca());
+        carro_1.acelerar();
+        carro_1.acelerar(80.0f);
+
+        carro_1.mostrarInfo();
     } 
 }
