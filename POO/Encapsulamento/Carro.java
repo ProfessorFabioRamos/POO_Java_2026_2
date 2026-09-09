53053147bdbf6fad69c9f44fc28f79b7f3cdcb09
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-09 10:36:42 -0300

    Update Carro.java

--- POO/Encapsulamento/Carro.java
+++ POO/Encapsulamento/Carro.java
@@ -1,32 +1,78 @@
-public class Carro {
+public class Carro{
+    // Atributos
     private String marca = "";
     private String modelo = "";
-    private int quantidadePassageiros = 0;
-    private float quilometragem = 0;
-    private float velocidadeAtual = 0;
     private String placa = "";
+    private int quantidadePassageiros = 0;
+    private float velocidadeAtual = 0;
+    private float quilometragem = 0;
     private boolean automatico = false;
 
-    public Carro(String marca, String modelo, int quantidadePassageiros,
-        String placa){
+    // Métodos
+    // Método Construtor
+    public Carro(String marca, String modelo, String placa,
+        int quantidadePassageiros){
         this.marca = marca;
         this.modelo = modelo;
-        this.quantidadePassageiros = quantidadePassageiros;
         this.placa = placa;
+        this.quantidadePassageiros = quantidadePassageiros;
     }
 
-    // Método
     public void acelerar(){
         System.out.println("VRUMMMM");
     }
-    // Sobrecarga do método acelerar()
+
     public void acelerar(float novaVelocidade){
         if(novaVelocidade < 220){
-            // Format de String = %d(int), %f(float e double), %s(String)
-            // %.2f = 2 casas decimais
-            String velText = String.format("Velocidade anterior: %f km/h, Velocidade Nova: %.2f km/h",velocidadeAtual,novaVelocidade );
             velocidadeAtual = novaVelocidade;
-            System.out.println(velText);
+            System.out.println("Velocidade: "+velocidadeAtual);
+        }
+    }
+
+    // Método Getter
+    public String getMarca(){
+        String marcaUpper = maiuscula(marca);
+        return marcaUpper;
+    }
+
+    // Método Setter
+    public void setMarca(String novaMarca){
+        if(novaMarca != null && !novaMarca.isEmpty()){
+            marca = novaMarca;
+        }
+        else{
+            System.out.println("Marca Inválida");
+        }
+    }
+    // Método privado (encapsulado)
+    private String maiuscula(String txt){
+        return txt.toUpperCase();
+    }
+
+    public int getQuantidadePassageiros() {
+        return quantidadePassageiros;
+    }
+
+    public void setQuantidadePassageiros(int novaQuantidade) {
+        if(novaQuantidade >= 1 && novaQuantidade <= 7){
+            quantidadePassageiros = novaQuantidade;
+        }
+        else{
+            System.out.println("Quantidade Inválida");
+        }
+       
+    }
+
+    public String getModelo(boolean lowerCaseMode) {
+        return lowerCaseMode? modelo.toLowerCase() : modelo;  // Operador Ternário
+    }
+
+    public void setModelo(String novoModelo) {
+        if(novoModelo != null && !novoModelo.isEmpty() && novoModelo.length() <= 30){
+            modelo = novoModelo;
+        }
+        else{
+            System.out.println("Modelo Inválido");
         }
     }
 
@@ -36,12 +82,7 @@ public class Carro {
         System.out.println("Placa: "+ placa);
         System.out.println("Passageiros: "+ quantidadePassageiros);
         System.out.println("Quilometragem: "+ quilometragem);
+        System.out.println("Velocidade Atual: "+ velocidadeAtual);
+        System.out.println("Câmbio Manual: "+ automatico);
     }
-
-    //Método Getter de marca
-    public String getMarca(){
-        return marca;
-    }
-
-    //Método Setter de marca
 }
