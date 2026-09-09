90295bc7eb13079cd637602df7b195502a315e73
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-09 10:36:16 -0300

    Update Main.java

--- POO/Encapsulamento/Main.java
+++ POO/Encapsulamento/Main.java
@@ -1,15 +1,22 @@
-public class Main{
+public class Main {
     public static void main(String[] args){
         Carro carro_1 = new Carro("Honda", "Civic",
-         5, "JGF65F90");
-        //System.out.println(carro_1.marca);
-        //carro_1.placa = "JOE65F90";
-        //System.out.println(carro_1.placa);
-
+        "JGF6F90", 5);
         System.out.println(carro_1.getMarca());
-        carro_1.acelerar();
-        carro_1.acelerar(80.0f);
-
+        carro_1.setMarca("");
+        carro_1.setMarca(null);
+        carro_1.setMarca("Toyota");
+        System.out.println(carro_1.getMarca());
+        
+        System.out.println(carro_1.getModelo(false));
+        System.out.println(carro_1.getModelo(true));
+        carro_1.setModelo("jhbfilawswswswswsdjkngvjksdbngjkawsenjkgawegwnggbf"); // Erro
+        carro_1.setModelo("Corolla");
+        System.out.println(carro_1.getModelo(false));
         carro_1.mostrarInfo();
-    } 
+
+        // Carro carro_2 = new Carro("Jeep", "Commander",
+        //     "MMH1K67", 7);
+        // System.out.println(carro_2.quantidadePassageiros);
+    }
 }
