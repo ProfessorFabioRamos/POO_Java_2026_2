5a04d3bdaffb7588ac6b00cc2d086c2998f83a55
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-14 10:29:20 -0300

    Update Main.java

--- POO/Encapsulamento/Main.java
+++ POO/Encapsulamento/Main.java
@@ -1,19 +1,29 @@
 public class Main {
     public static void main(String[] args){
         Carro carro_1 = new Carro("Honda", "Civic",
-        "JGF6F90", 5);
+         5, "JGF65F90");
+        //System.out.println(carro_1.marca);
+        //carro_1.placa = "JOE65F90";
+        //System.out.println(carro_1.placa);
+
         System.out.println(carro_1.getMarca());
-        carro_1.setMarca("");
         carro_1.setMarca(null);
+        carro_1.setMarca("");
         carro_1.setMarca("Toyota");
         System.out.println(carro_1.getMarca());
-        
-        System.out.println(carro_1.getModelo(false));
+
+        System.out.println(carro_1.getQuantidadePassageiros());
+        carro_1.setQuantidadePassageiros(0);
+        carro_1.setQuantidadePassageiros(10);
+        carro_1.setQuantidadePassageiros(7);
+        System.out.println(carro_1.getQuantidadePassageiros());
+
         System.out.println(carro_1.getModelo(true));
-        carro_1.setModelo("jhbfilawswswswswsdjkngvjksdbngjkawsenjkgawegwnggbf"); // Erro
+        carro_1.setModelo(null);
+        carro_1.setModelo("");
+        carro_1.setModelo("jkdsfkjhadkjfhsadkjhfjkadshfhdsajkfhsdjk");
         carro_1.setModelo("Corolla");
         System.out.println(carro_1.getModelo(false));
-        carro_1.mostrarInfo();
 
         // Carro carro_2 = new Carro("Jeep", "Commander",
         //     "MMH1K67", 7);
