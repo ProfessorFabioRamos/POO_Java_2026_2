5e30ccafeabdfc03245d3c6789544a0944f509f7
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-16 10:44:05 -0300

    Create Cachorro.java

---
+++ POO/Heranca/Cachorro.java
@@ -0,0 +1,21 @@
+public class Cachorro extends Animal{
+    public String raca;
+    
+    public Cachorro(String nome, int idade, String raca){
+        // Faz referência ao método construtor da classe super
+        super(nome, idade);
+        this.raca = raca;
+    }
+
+    // Sobrescrita (Polimorfismo)
+    @Override 
+    public void emitirSom(){
+        System.out.println("Au Au");
+    }
+
+    @Override 
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Raça:"+raca);
+    }
+}
