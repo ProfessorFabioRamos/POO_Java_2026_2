4a1e733f0f68edf12b32525efe39a7eb9d6ea160
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-16 10:44:27 -0300

    Create Animal.java

---
+++ POO/Heranca/Animal.java
@@ -0,0 +1,20 @@
+// Classe Base ou Super
+public class Animal {
+    public String nome = "";
+    public int idade = 0;
+    
+    public Animal(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Método Genérico
+    public void emitirSom(){
+        System.out.println("Animal disse:"+nome);
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Nome:"+nome); 
+        System.out.println("Idade:"+idade); 
+    }
+}
