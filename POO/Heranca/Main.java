1c786da6a5e42f23312efa7efb9036239940105b
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-16 10:43:44 -0300

    Create Main.java

---
+++ POO/Heranca/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        //Animal animal_1 = new Animal("Ornitorrinco", 4);
+        //animal_1.emitirSom();
+        //animal_1.mostrarInfo();
+
+        Cachorro cachorro_1 = new Cachorro("Totó", 3, "Labrador");
+        cachorro_1.emitirSom();
+        cachorro_1.mostrarInfo();
+        //System.out.println("Raça:"+cachorro_1.raca);
+    }
+}
