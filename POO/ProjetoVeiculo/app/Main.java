266aa7c9f49cb2b37746ab8fa4dd0a06687a61ca
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-23 10:53:48 -0300

    Create Main.java

---
+++ POO/ProjetoVeiculo/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+import veiculos.Veiculo;
+
+public class Main {
+    public static void main(String[] args){
+        // Não é permitido instanciar objeto da classe Veiculo
+        //Veiculo veiculo_1 = new Veiculo("Patinete");
+    }
+}
